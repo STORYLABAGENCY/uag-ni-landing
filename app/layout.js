1c7eb4e802058819b2d17aa6a456065a04780b2e
sistemabanuelos-wq <sistemabanuelos@gmail.com>
2026-07-01 17:33:11 -0600

    GTM-5T5PJ7KN en layout NI

--- app/layout.js
+++ app/layout.js
@@ -8,14 +8,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5T5PJ7KN');` }} />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700;800;900&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" />
       </head>
       <body style={{ margin: 0, padding: 0 }}>
-        {/* Google Tag Manager (noscript) */}
         <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5T5PJ7KN" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
         {children}
       </body>
